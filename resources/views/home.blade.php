2ce4dfa19aef52065a1586389b498d5309b38e32
DavideGila <davide.gila@gmail.com>
2023-12-18 18:08:13 +0100

    added style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
         <div class="container d-flex justify-content-between align-items-center p-3">
             @foreach ($content as $el)
             <img src="{{$el['img']}}" alt="" class="blue-bar-image">
-            <span class="p-2 text-uppercase">{{ $el['text'] }}</span>
+            <span class="p-2 text-uppercase text-light">{{ $el['text'] }}</span>
             @endforeach
         </div>
     </div>
