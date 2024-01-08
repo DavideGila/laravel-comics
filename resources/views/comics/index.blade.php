eabb04c54e8b7ce3267c32f05acabc857725ff11
DavideGila <davide.gila@gmail.com>
2024-01-08 14:29:23 +0100

    modified foreach

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,14 +8,14 @@
         <div class="container">
             <span class="text-light bg-primary p-3 fs-3 text-uppercase">Current Series</span>
             <div class="row">
-                @foreach ($product as $key => $el)
+                @foreach ($comics as $comic)
                     <div class="col-2 py-5">
                         <div>
-                            <img src="{{ $el['thumb'] }}" alt="" class="object-fit-cover series-img">
+                            <img src="{{ $comic->thumb }}" alt="" class="object-fit-cover series-img">
                             <div>
-                                <h6 class="title text-light text-uppercase">{{ $el['title'] }}</h6>
+                                <h6 class="title text-light text-uppercase">{{ $comic->title }}</h6>
                             </div>
-                            <div><a href="{{route('comics.show', $key)}}" class="btn btn-primary">Read More</a></div>
+                            <div><a href="{{route('comics.show', $comic->id)}}" class="btn btn-primary">Read More</a></div>
                         </div>
                     </div>
                 @endforeach
@@ -25,13 +25,13 @@
             </div>
         </div>
     </div>
-    <div class="bg-primary d-flex py-4">
+    {{-- <div class="bg-primary d-flex py-4">
         <div class="container d-flex justify-content-between align-items-center p-3">
             @foreach ($content as $el)
-            <img src="{{$el['img']}}" alt="" class="blue-bar-image">
-            <span class="p-2 text-uppercase text-light">{{ $el['text'] }}</span>
+            <img src="{{$el->thumb}}" alt="" class="blue-bar-image">
+            <span class="p-2 text-uppercase text-light">{{ $el->text }}</span>
             @endforeach
         </div>
-    </div>
+    </div> --}}
 </main>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,27 +4,19 @@
 
 @section('content')
 <main class="bg-dark">
-    <section class="container mb-4">
-        <h1 class="text-white">{{$product['title']}}</h1>
+    <section class="container py-4">
+        <h1 class="text-white">{{$comic->title}}</h1>
         <div class="row gy-4">
             <div class="col-12 w-50">
                 <div class="card">
-                    <img src="{{$product['thumb']}}" alt="{{$product['title']}}" class="card-img-top">
+                    <img src="{{$comic->thumb}}" alt="{{$comic->title}}" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">{{$product['title']}}</h5>
-                        <p class="card-text">{{$product['description']}}</p>
+                        <h5 class="card-title">{{$comic->title}}</h5>
+                        <p class="card-text">{{$comic->description}}</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <div class="bg-primary d-flex py-4">
-        <div class="container d-flex justify-content-between align-items-center p-3">
-            @foreach ($content as $el)
-            <img src="{{$el['img']}}" alt="" class="blue-bar-image">
-            <span class="p-2 text-uppercase text-light">{{ $el['text'] }}</span>
-            @endforeach
-        </div>
-    </div>
 </main>
 @endsection
